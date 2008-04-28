43843869f4ec44c398bef7750874d60ff398075c
Ben Roberts <ben@sihnon.net>
2008-04-28 20:42:58 +0000

    Added 3yp and qmc pages. Removed .htaccess (sites upgrading past this revision will need to backup and restore their customised .htaccess files)

--- page-sources/projects.php
+++ page-sources/projects.php
@@ -9,5 +9,9 @@
 
 ?>
 <p>
-	<em>I have not yet written up the list of projects I am working on</em>
+	Some of the projects I have worked on:
+	<ul>
+		<li><a href="<?php echo $_req->construct("page", "3yp"); ?>" title="Third Year Project">Third Year Project</a></li>
+		<li><a href="<?php echo $_req->construct("page", "qmc"); ?>" title="Qt Media Centre">Qt Media Centre</a></li><b></b>
+	</ul>
 </p>
---
+++ page-sources/qmc.php
@@ -0,0 +1,13 @@
+<?php
+
+	/*
+	 *	Qt Media Centre
+	 * 
+	 */
+	 
+	 $_template['title'] = "Third Year Project";
+
+?>
+<p>
+	<em>Details of this project have not been released yet.</em>
+</p>
