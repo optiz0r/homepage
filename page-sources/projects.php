ada7e13d862e7dbc262b0202246e17a9aff68308
Ben Roberts <ben@sihnon.net>
2008-10-28 23:45:32 +0000

    Qt Media Centre never happened then.

--- page-sources/projects.php
+++ page-sources/projects.php
@@ -12,6 +12,5 @@
 	Some of the projects I have worked on:
 	<ul>
 		<li><a href="<?php echo $_req->construct("page", "3yp"); ?>" title="Third Year Project">Third Year Project</a></li>
-		<li><a href="<?php echo $_req->construct("page", "qmc"); ?>" title="Qt Media Centre">Qt Media Centre</a></li><b></b>
 	</ul>
 </p>
