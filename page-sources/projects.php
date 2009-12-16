12c22a73e1af43279cf264194f317f3a88494abc
Ben Roberts <ben@sihnon.net>
2009-12-16 23:01:34 +0000

    Updated link to the wiki, to point at miranda

--- page-sources/projects.php
+++ page-sources/projects.php
@@ -12,7 +12,7 @@
 	Some of the projects I have worked on:
     <ul>
         <li><a href="<?php echo $_req->construct("page", "overlay"); ?>" title="Gentoo Overlay">Gentoo Portage Overlay</a></li>
-        <li><a href="https://dev.sihnon.net/wiki" title="Sihnon Wiki">Sihnon Wiki</a></li>
+        <li><a href="https://wiki.sihnon.net/" title="Sihnon Wiki">Sihnon Wiki</a></li>
 		<li><a href="<?php echo $_req->construct("page", "3yp"); ?>" title="Third Year Project">Third Year Project</a></li>
 	</ul>
 </p>
