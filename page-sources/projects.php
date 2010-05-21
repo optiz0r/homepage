541fdd1916a24a063a4bdfee9671c6e8ac1ea629
Ben Roberts <ben@saffron.sihnon.net>
2010-05-21 19:26:18 +0100

    Updated projects page Added Handbrake projects Added one-line descriptions to each project link

--- page-sources/projects.php
+++ page-sources/projects.php
@@ -8,11 +8,26 @@
 	 $_template['title'] = "Projects";
 
 ?>
-<p>
-	Some of the projects I have worked on:
-    <ul>
-        <li><a href="<?php echo $_req->construct("page", "overlay"); ?>" title="Gentoo Overlay">Gentoo Portage Overlay</a></li>
-        <li><a href="https://wiki.sihnon.net/" title="Sihnon Wiki">Sihnon Wiki</a></li>
-		<li><a href="<?php echo $_req->construct("page", "3yp"); ?>" title="Third Year Project">Third Year Project</a></li>
-	</ul>
-</p>
+<h2>Development projects:</h2>
+<dl>
+    <dt><a href="<?php echo $_req->construct("page", "overlay"); ?>" title="Gentoo Overlay">Gentoo Portage Overlay</a></dt>
+    <dd>Personally developed software, or miscellaneous ebuilds that can't be found in any other overlay.</dd>
+
+    <dt><a href="https://wiki.sihnon.net/" title="Sihnon Wiki">Sihnon Wiki</a></dt>
+    <dd>Documentation for various systems I've configured; mostly for personal reference, but may be useful to others.</dd>
+
+    <dt><a href="https://git.sihnon.net/cgit.cgi/dev.git/tree/handbrake-cluster" title="HandBrakeCluster">HandBrakeCluster</a></dt>
+    <dd>A collection of perl scripts to use HandBrakeCLI to batch rip DVD images using multiple machines.</dd>
+
+    <dt><a href="https://git.sihnon.net/cgit.cgi/handbrake-cluster-webui.git/" title="HandBrakeCluster WebUI">HandBrakeCluster WebUI</a></dt>
+    <dd>An alternative web-based UI for the HandBrakeCluster scripts, written in PHP.</dd>
+
+</dl>
+
+
+<h2>University projects:</h2>
+<dl>
+    <dt><a href="<?php echo $_req->construct("page", "3yp"); ?>" title="Third Year Project">Third Year Project</a></dt>
+    <dd>A cross-platform, zero-config file sharing client using public keys and a web of trust for password-less authentication and access control.</dd>
+</dl>
+
