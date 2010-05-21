0dd6c285c2a6a3f1ebd35b6c4a6711a95534844d
Ben Roberts <ben@saffron.sihnon.net>
2010-05-21 19:24:33 +0100

    Updated the homepage text and avatar image

--- page-sources/home.php
+++ page-sources/home.php
@@ -9,19 +9,22 @@
     
 ?>
 <div>
-<img class="avatar" style="width: 10em;" src="<?php echo $_meta['base-dir']; ?>/files/me-300.png" alt="Photo of Ben Roberts" />
+<img class="avatar" style="width: 10em;" src="<?php echo $_meta['base-dir']; ?>/files/portrait.jpg" alt="Photo of Ben Roberts" />
 	<p>
-		I am part way through the final year of a Master's degree in Computer Science at ECS, University of Southampton, UK. I am due to graduate in June 2010.
+		I have almost finished the final year of a Master's degree in Computer Science at ECS, University of Southampton, UK and am due to graduate in June 2010.
+    </p>
+    <p>
+        Starting this summer, I will be working for <a href="http://www.uk.atosorigin.com/en-uk/" title="Atos Origin UK homepage">Atos Origin</a> as a <em>Graduate Technical Specialist</em> in the <em>Managed Operations</em> division.
+    </p>
+	<p>
+		I previously worked for <a href="http://www.netcraft.com/" title="Netcraft homepage">Netcraft</a> in Bath, while taking a year out from my degree studies. My roles included developing and running the <a href="http://news.netcraft.com/SSL-survey" title="Netcraft's SSL Server Survey">SSL Server Survey</a>, reviewing <a href="http://audited.netcraft.com/audited" title="Netcraft's Automated Vulnerability Scanning">Automated Vulnerability Scan</a> results, and performing occasional <a href="http://audited.netcraft.com/web-application" title="Netcraft's Application Penetration & Security Testing">penetration tests</a> against web applications for financial institutions.
 	</p>
 	<p>
-		I spent the last year working for <a href="http://www.netcraft.com/" title="Netcraft homepage">Netcraft</a> in Bath, while taking a year out from my studies. My roles included developing and running the <a href="http://news.netcraft.com/SSL-survey" title="Netcraft's SSL Server Survey">SSL Server Survey</a>, reviewing <a href="http://audited.netcraft.com/audited" title="Netcraft's Automated Vulnerability Scanning">Automated Vulnerability Scan</a> results, and doing the occasional <a href="http://audited.netcraft.com/web-application" title="Netcraft's Application Penetration & Security Testing">penetration test</a> against web applications for financial institutions.
-	</p>
-	<p>
-		On this site you can find a copy of my <a href="<?php echo $_meta['base-dir']; ?>/files/BenRobertsCv.pdf" title="Curriculum Vitae">CV</a>,
+		On this site you can find a full copy of my <a href="<?php echo $_meta['base-dir']; ?>/files/BenRobertsCv.pdf" title="Curriculum Vitae">CV</a>,
 		or see what <a href="<?php echo $_req->construct('page','projects'); ?>" title="Projects">projects</a> I have been working on in my spare time.
 	</p>
 	<p>
-		You can contact me via bar105<img class="at" src="<?php echo $_meta['base-dir']; ?>/resources/at.png" alt="@" />ecs.soton.ac.uk.
+		You can contact me via me<img class="at" src="<?php echo $_meta['base-dir']; ?>/resources/at.png" alt="@" />benroberts.net.
 	</p>
 	
 	
